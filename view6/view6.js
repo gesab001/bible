edc96bf6bea57d0c8354828a88e3f0ed97f76287
gesab0001 <144000giovanni@gmail.com>
2021-06-21 19:13:32 +0000

    added modal to memoryverse

--- view6/view6.js
+++ view6/view6.js
@@ -16,128 +16,22 @@ app.config(['$routeProvider', function($routeProvider) {
 	cssInjector.add("view6/view6.css");
     $scope.selectedTopic = "show all";
     $scope.bookData = {};
+	$scope.memoryverseindex = 0;
     $scope.kjv = "";
-       $http.get("https://20.70.176.210/php/bible/select.php")
+    $http.get("https://20.70.176.210/php/bible/select.php")
        .then(function(response) {
          $scope.kjv = response.data;
          $scope.bookData = $scope.kjv;
          $scope.loading = false;
 		 console.log($scope.bookData);
-
        }, function(response) {
                $scope.kjv = response.data || 'Request failed';
     });
-    $scope.selectedTopicChanged = function(){
-        $scope.bookData = {};
-        if ($scope.selectedTopic=="show all"){
-            $scope.bookData = $scope.kjv;
-        }else{
-            $scope.bookData[$scope.selectedTopic] = $scope.kjv[$scope.selectedTopic];
-        }
-
-    }
-    $scope.booknumbers = "";  
-    $scope.id = getCurrentID();
-    //get booklist
-    $http.get("assets/booklist2.json")
-    .then(function(response) {
-      $scope.bibles = response.data.books;
-      $scope.versionoptions = response.data.versionoptions;
-      $scope.selectedVersion = "kjv";
-         //get kjv bible
-        
-    }, function(response) {
-            $scope.bibles = response.data.books || 'Request failed';
-    });
     
- 
-    $scope.getWord = function (book, id){
-        $scope.totalverses = $scope.kjv[book].length;
-        $scope.currentID = id%$scope.totalverses;
-        $scope.currentVerse = $scope.kjv[book][$scope.currentID];
-        $scope.chapterNumber = $scope.currentVerse.chapter;
-        $scope.verseNumber = $scope.currentVerse.verse;
-        return $scope.currentVerse.word;
-    };
-   $scope.translate = function(translationversion, title, bookname, id){
-        $scope.translatebooknumber = $scope.bibles[bookname];   
-        if (translationversion=="textusreceptus" && $scope.translatebooknumber<40){
-                $scope.kjv[title][id].word = "no translation for this version. Try Greek OT";  
-
-        }else if (translationversion=="greekot" && $scope.translatebooknumber>39) {
-                $scope.kjv[title][id].word = "no translation for this version. Try Greek Textus Receptus";  
-
-        }else if (translationversion=="hebrew" && $scope.translatebooknumber>39) {
-                $scope.kjv[title][id].word = "no translation for this version. Try Modern Hebrew";  
-        }
-        else{
-            $scope.kjv[title][id].word = "translating... please wait";  
-            $scope.translatebook = $scope.kjv[title];
-            $scope.translatetotalverses = $scope.translatebook.length;
-            $scope.translatecurrentid = id;
-            $scope.translatecurrentVerse = $scope.translatebook[$scope.translatecurrentid];
-            $scope.translatebooknumber = $scope.bibles[bookname];
-            $scope.translatechapter = $scope.translatecurrentVerse.chapter;
-            $scope.translateverse = $scope.translatecurrentVerse.verse;
-
-            $http.get("assets/versions/"+translationversion+"-version.json")
-                  .then(function(response) {
-                    $scope.translationbible = response.data;
-                    try{
-                        $scope.translatedword = $scope.translationbible["version"][$scope.translatebooknumber]["book"][$scope.translatechapter]["chapter"][$scope.translateverse]["verse"];
-                        $scope.kjv[title][id].word = $scope.translatedword;
-
-
-                    }catch(err){
-                          $scope.kjv[title][id].word = "Sorry. no translation for this version. Perhaps this verse is not available in the King James Version.";      
-                    }
-
-
-                  }, function(response) {
-                          $scope.translation = response.data || 'Request failed';
-               }); 
-        }
-    
-//        
-         //get kjv bible
-        
+   
+      $scope.setMemoryVerseIndex = function(index){
+        $scope.memoryverseindex = index;
   
-   };
-    $scope.theTime = new Date().toLocaleTimeString();
-    $interval(function () {
-        $scope.theTime = new Date();
-        $scope.id = getCurrentID();
-    }, 1000);
-    //get versions list
-//    $http.get("view6/versions_json.json")
-//    .then(function(response) {
-//
-//      $scope.versionslist = response.data.versionslist;
-//      $scope.versionoptions = response.data.versionoptions;
-//      for (var v of $scope.versionslist){
-//          
-//          $scope.versions.choices.push({"version": $scope.versionoptions[v], "code": v});
-//      }
-//      $scope.id = getCurrentID();
-//      
-//    });
-//    $scope.theTime = new Date().toLocaleTimeString();
-//    $interval(function () {
-//        $scope.theTime = new Date();
-//        $scope.id = getCurrentID()
-//    }, 1000);
-    
- 
-    
+    };  
     
 }); 
-
-function getCurrentID(){
-
-    var date1 = new Date();
-    var date2 = new Date(2018, 5, 23, 14, 45, 0, 0);
-    var difference = date1.getTime() - date2.getTime();
-    var minutesDifference = Math.floor(difference/1000/60);
-    var currentID=minutesDifference;
-    return currentID;
-}
